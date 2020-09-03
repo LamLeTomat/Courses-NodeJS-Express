6cb4a81d1060a256cf401e9ea58ab119272fd49c
LamLeTomat <thunderbrucelee@gmail.com>
2020-09-03 11:51:18 +0700

    setup static files path and SCSS

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars  = require('express-handlebars');
 
 const port = 3000;
 
+//set static files public/
+app.use(express.static(path.join(__dirname, 'public')));
 
 //template engine
 app.engine('hbs', handlebars({
@@ -13,9 +15,11 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
+//route
 app.get('/', (req, res) => {
   res.render('home');
 });
+
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
